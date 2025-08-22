e70e2b92279e088c88bd7be7209ce745f97f27f0
Vitalii Rasiak <rasjakv020303@gmail.com>
2025-08-22 15:31:32 +0300

    Add /ask endpoint

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,8 @@ app = FastAPI(title="Movies/Series RAG Chatbot")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.get("/ask")
+async def ask(): 
+    return {"message":"data"}
